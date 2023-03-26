72b1dbbc5f501a00822231c13ca3672f630a45a5
mahede182 <mahede.object@gmail.com>
2023-03-26 23:23:48 +0600

    Link :boom:

---
+++ Components/LinkPrac.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'native-base';
+
+const LinkPrac = () => {
+    return <Box alignItems="center">
+      <Link href="https://nativebase.io">
+        Click here to open documentation.
+      </Link>
+    </Box>;
+}
+export default LinkPrac;
